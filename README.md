a8ed3a8bae2e49cc90c3f9a4651a6aaa13289aa4
Kenji Urazaki Junior <39385029+kjrurazaki@users.noreply.github.com>
2024-08-20 14:57:30 +0000

    add `lint:check`and `lint:fix` scripts

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Learning curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 function Home() {
-    return(
-    <h1>🔮
-    Trials shape the mind,<br />
-    Queries bloom, answers unwind—<br />
-    Wisdom intertwined.
-    </h1>)
+  return (
+    <h1>
+      🔮 Trials shape the mind,
+      <br />
+      Queries bloom, answers unwind—
+      <br />
+      Wisdom intertwined.
+    </h1>
+  );
 }
 
 export default Home;
